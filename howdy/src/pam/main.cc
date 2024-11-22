4ce95bc305388822a452324db74c0e285e8609d2
Osilvfe <3072756864@qq.com>
2024-11-22 22:54:34 +0800

    try to fix dismiss lockscreen bugs again4

--- howdy/src/pam/main.cc
+++ howdy/src/pam/main.cc
@@ -147,7 +147,7 @@ auto howdy_status(char *username, int status, const INIReader &config,
   syslog(LOG_INFO, "Login approved");
   
   if (config.GetBoolean("core", "dismiss_lockscreen", true)){
-    system("echo -e '\n'");
+    system("xdotool type ' '");
   }
 
   return PAM_SUCCESS;
