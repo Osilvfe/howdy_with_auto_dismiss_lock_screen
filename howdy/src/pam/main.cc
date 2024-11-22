57c9cfe31ca2835a1af7fe2e1d8eeaa4f29ec946
Osilvfe <3072756864@qq.com>
2024-11-22 22:09:14 +0800

    try to fix dismiss lockscreen bugs again

--- howdy/src/pam/main.cc
+++ howdy/src/pam/main.cc
@@ -147,7 +147,7 @@ auto howdy_status(char *username, int status, const INIReader &config,
   syslog(LOG_INFO, "Login approved");
   
   if (config.GetBoolean("core", "dismiss_lockscreen", true)){
-    system("sleep 1");
+    system("sleep 2");
     simulatespace();
   }
 
